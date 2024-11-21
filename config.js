d292d316c1f4cc203859fda452e12bdbf09ab44b
thierrypdamiba <thierry.damiba@qdrant.com>
2024-11-21 06:59:30 -0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  accountId: '',
-  clientId: '',
-  clientSecret: '',
-  zoomApiBaseUrl: 'https://api-endpoint-0f24e0ac73d6.herokuapp.com',
-  anthropic_api_key: '',
-};
-
-module.exports = config;
